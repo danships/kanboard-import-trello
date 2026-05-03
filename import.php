dc5b10406c2bc220edb02da3f26eeb51448ebe67
Andreas Dangel <andreas.dangel@adangel.org>
2026-05-03 09:16:34 +0200

    Various improvements (#17)
    * Fix attachment downloading with oauth
    
    * Fix trello labels to category conversion
    
    Trello labels might have an empty name, which is not allowed for
    categories in kanban. Using the colorId as the name then.
    
    * Better logging, error handling
    
    * Add user as project-manager
    
    This allows to set the user as creator of the tasks.
    
    * Count attachments and sizes
    
    * Add original trello user info to cards and comments
    
    Note: this needs php extension "intl"
    
    * Remove deprecated curl_close call

--- import.php
+++ import.php
@@ -26,7 +26,7 @@ if ($argc !== 6 && $argc !== 7) {
 	echo 'Use this small tool to import your Trello JSON export into your kanboard, using it JSON-RPC interface.' . PHP_EOL;
 	printf('Usage: php %s http://server/jsonrpc.php apitoken trellokey trellotoken trelloboard [userId]%s', $argv[0], PHP_EOL);
 	echo 'To get the Trello key and token, login to Trello and go to https://trello.com/app-key'.PHP_EOL;
-	echo 'The user id is optional. If you provide it, comments will be created with that userId.'.PHP_EOL;
+	echo 'The user id is optional. If you provide it, cards and comments will be created with that userId.'.PHP_EOL;
 	die;
 }
 
@@ -39,12 +39,16 @@ $userId = null;
 if (isset($argv[6])) {
 	$userId = $argv[6];
 }
+// Adds the original trello user info to cards and comments
+$addTrelloUserInfo = TRUE;
 
 $jsonString = file_get_contents("https://trello.com/1/boards/".$trelloboard."?key=".$trellokey."&token=".$trellotoken);
 $trelloObj = json_decode($jsonString);
 if (empty($trelloObj)) {
 	printf($jsonString);
-	printf('Unable to parse JSON response, is it valid? %s', json_last_error_msg());
+	echo PHP_EOL;
+	printf('Unable to parse JSON response for trelloObj, is it valid? %s', json_last_error_msg());
+	echo PHP_EOL;
 	die(1);
 }
 
@@ -72,18 +76,45 @@ if (!is_array($projects)) {
 	die(1);
 }
 
+$dateFormatter = new IntlDateFormatter(
+		#'de_DE',
+		'en_US',
+		IntlDateFormatter::LONG,
+		IntlDateFormatter::LONG,
+		#'Europe/Berlin',
+		'America/Los_Angeles',
+		IntlDateFormatter::GREGORIAN
+);
+
+
+//stats
+$attachmentCount = 0;
+$attachmentTotalSizeInBytes = 0;
+
 //variables
 $trelloLists = array();
 $trelloLabels = array(); //we will store all label names, but not add them immediately, only when used
 $trelloAttachments = array();
+$trelloUsers = array();
 
 //create the project
-echo 'Creating project.' . PHP_EOL;
+echo "Creating project '" . $trelloObj->name . "' ..." . PHP_EOL;
 $projectId = $client->createProject($trelloObj->name);
 $counter=0;
 while (empty($projectId)) {
 $projectId = $client->createProject($trelloObj->name.$counter++);
 //  die("We could not create the project, perhaps it already exists?".PHP_EOL);
+}
+echo "Created project '" . $trelloObj->name . "' (projectId=$projectId)" . PHP_EOL;
+
+if ($userId !== null) {
+	$projectPermission = $client->addProjectUser($projectId, $userId, 'project-manager');
+	if ($projectPermission === TRUE) {
+		echo " Add user $userId as project-manager" . PHP_EOL;
+	} else {
+		echo " Could not add user $userId as project-manager!" . PHP_EOL;
+		die(1);
+	}
 }
 
 //remove the columns created by default
@@ -101,23 +132,44 @@ if ($trelloObj->prefs->permissionLevel=="private") {
 # will only get lists that are not archived
 $jsonString = file_get_contents("https://trello.com/1/boards/".$trelloboard."/lists?key=".$trellokey."&token=".$trellotoken);
 $trelloObjLists = json_decode($jsonString);
+if (empty($trelloObjLists)) {
+	printf($jsonString);
+	echo PHP_EOL;
+	printf('Unable to parse JSON response for trelloObjLists, is it valid? %s', json_last_error_msg());
+	echo PHP_EOL;
+	die(1);
+}
+
 
 //add the lists
-echo 'Adding lists.' . PHP_EOL;
+echo "Found " . count($trelloObjLists) . " lists" . PHP_EOL;
 foreach ($trelloObjLists as $list) {
 	if ($list->closed) {
 		// ignore archived lists
+		echo "  List {$list->name} is closed/archived. Ignored!" . PHP_EOL;
 		continue;
 	}
-	echo 'Creating list '.$list->name.PHP_EOL;
+	echo '  Creating list "' . $list->name . '"' . PHP_EOL;
 	$columnId = $client->addColumn($projectId, $list->name);
+	if ($columnId === false) {
+		echo "Error creating column! (projectId=$projectId, name='{$list->name}')" . PHP_EOL;
+		die(1);
+	}
 	$trelloLists[$list->id] = $columnId;
 
 	//add each card
-	echo 'Adding cards.' . PHP_EOL;
-        $query="https://trello.com/1/lists/".$list->id."?key=".$trellokey."&token=".$trellotoken."&cards=open&card_fields=all&card_checklists=all&members=all&member_fields=all&membersInvited=all&checklists=all&organization=true&organization_fields=all&fields=all"; // &actions=commentCard,copyCommentCard&card_attachments=true";
-        $jsonCards = file_get_contents($query);
+	$query="https://trello.com/1/lists/".$list->id."?key=".$trellokey."&token=".$trellotoken."&cards=open&card_fields=all&card_checklists=all&members=all&member_fields=all&membersInvited=all&checklists=all&organization=true&organization_fields=all&fields=all"; // &actions=commentCard,copyCommentCard&card_attachments=true";
+	$jsonCards = file_get_contents($query);
 	$trelloObjCards = json_decode($jsonCards);
+	if (empty($trelloObjCards)) {
+		printf($jsonString);
+		echo PHP_EOL;
+		printf('Unable to parse JSON response for trelloObjCards, is it valid? %s', json_last_error_msg());
+		echo PHP_EOL;
+		die(1);
+	}
+
+	echo "  Found " . count($trelloObjCards->cards) . " cards..." . PHP_EOL;
 
 	foreach ($trelloObjCards->cards as $card) {
 		addCard($projectId, $columnId, $card);
@@ -125,7 +177,53 @@ foreach ($trelloObjLists as $list) {
 }
 
 echo 'All done!' . PHP_EOL;
+echo "Project '" . $trelloObj->name . "' (projectId=$projectId)" . PHP_EOL;
+echo "  Number of attachments: {$attachmentCount}" . PHP_EOL;
+echo "  Total size of attachments: {$attachmentTotalSizeInBytes} bytes" . PHP_EOL;
 die;
+
+function resolveTrelloUserId($idMember)
+{
+global $trellokey;
+global $trellotoken;
+global $trelloUsers;
+	if (isset($trelloUsers[$idMember])) {
+		return $trelloUsers[$idMember];
+	}
+
+	echo "- Resolving trello user id {$idMember}..." . PHP_EOL;
+	$jsonString = 
+			@file_get_contents("https://api.trello.com/1/members/".$idMember.
+				"?key=".$trellokey."&token=".$trellotoken."&fields=id,fullName,username");
+	if ($jsonString === FALSE) {
+		$memberDetails = "unknown";
+	} else {
+		$memberDetails = json_decode($jsonString);
+		if (empty($memberDetails)) {
+			printf($jsonString);
+			echo PHP_EOL;
+			printf('Unable to parse JSON response for memberDetails, is it valid? %s', json_last_error_msg());
+			echo PHP_EOL;
+			die(1);
+		}
+		$memberDetails = "{$memberDetails->username} ({$memberDetails->fullName})";
+	}
+	$trelloUsers[$idMember] = $memberDetails;
+	echo "  -> {$memberDetails}" . PHP_EOL;
+	return $memberDetails;
+}
+
+function formatActionMemberCreator($action)
+{
+global $dateFormatter;
+	if ($action->memberCreator) {
+		$memberDetails = "{$action->memberCreator->username} ({$action->memberCreator->fullName})";
+	} else {
+		$memberDetails = "unknown";
+	}
+	$formattedDate = $dateFormatter->format(new DateTimeImmutable($action->date));
+	return "by {$memberDetails} on {$formattedDate}";
+}
 
 function addCard($projectId, $columnId, $card)
 {
@@ -134,10 +232,40 @@ global $trellotoken;
 global $trelloLabels;
 global $client;
 global $userId;
+global $addTrelloUserInfo;
+global $dateFormatter;
 
 	if ($card->closed) {
 		// ignore archived cards
+		echo "    card '{$card->name}' is closed/archived -> ignored." . PHP_EOL;
 		return;
+	}
+
+	# read all actions: https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-boardid-actions-get
+	# types: https://developer.atlassian.com/cloud/trello/guides/rest-api/action-types/
+	# interesting types: createCard, commentCard, copyCard, copyCommentCard
+	$jsonString = 
+		file_get_contents("https://trello.com/1/cards/".
+			$card->shortLink."/actions?key=".$trellokey."&token=".$trellotoken."&filter=createCard,commentCard,copyCard,copyCommentCard&memberCreator=true&memberCreator_fields=fullName,username&limit=200");
+	$cardActions = json_decode($jsonString);
+	if (empty($cardActions)) {
+		print_r($card);
+		printf($jsonString);
+		echo PHP_EOL;
+		printf('Unable to parse JSON response for cardActions, is it valid? %s', json_last_error_msg());
+		echo PHP_EOL;
+		die(1);
+	}
+
+	$createdInfo = "";
+	if ($addTrelloUserInfo === TRUE) {
+		foreach($cardActions as $action) {
+			if ($action->type === 'createCard') {
+				$createdInfo = "\n\n--\nCreated " . formatActionMemberCreator($action);
+			} else if ($action->type === 'copyCard') {
+				$createdInfo = "\n\n--\nCopied " . formatActionMemberCreator($action);
+			}
+		}
 	}
 
 	$dueDate = $card->due !== null ? date('Y-m-d', strtotime($card->due)) : null;
@@ -151,7 +279,17 @@ global $userId;
 		if (isset($trelloLabels[$trelloLabel->id])) {
 			$categoryId = $trelloLabels[$trelloLabel->id];
 		} else {
-			$categoryId = $client->createCategory($projectId, $trelloLabel->name);
+			$name = $trelloLabel->name;
+			if ($name == "") {
+				$name = "({$colorId})";
+			}
+			$categoryId = $client->createCategory($projectId, $name);
+			if ($categoryId === false) {
+				echo "Error creating category! projectId=$projectId, name='$name'" . PHP_EOL;
+				print_r($card);
+				die(1);
+			}
+			echo "    Created category id=$categoryId projectId=$projectId name='$name'" . PHP_EOL;
 			$trelloLabels[$trelloLabel->id] = $categoryId;
 		}
 	}
@@ -162,7 +300,7 @@ global $userId;
 		'column_id' => $columnId
 	);
 	if ($card->desc !== null) {
-		$params['description'] = $card->desc;
+		$params['description'] = $card->desc . $createdInfo;
 	}
 	if ($dueDate !== null) {
 		$params['date_due'] = $dueDate;
@@ -173,25 +311,30 @@ global $userId;
 	if ($categoryId !== null) {
 		$params['category_id'] = $categoryId;
 	}
-	//TODO temporary disabled, seems to cause errors when addings tasks in later Kanboard versions >1.0.30
-	/*if ($userId !== null) {
-		$params['owner_id'] = $userId;
-	}*/
-	$taskId = $client->createTask($params);
-
-	if ($card->badges->comments > 0) {
-		$jsonString = 
-			file_get_contents("https://trello.com/1/cards/".
-				$card->shortLink."/actions?key=".$trellokey."&token=".$trellotoken."&filter=all");
-		$cardDetails = json_decode($jsonString);
-		addComments($cardDetails, $taskId);
+	if ($userId !== null) {
+		$params['creator_id'] = $userId;
 	}
+	$taskId = $client->createTask($params);
+	if ($taskId === false) {
+		echo "Error creating task! " . print_r($params, true) . PHP_EOL;
+		die(1);
+	}
+	echo '    Added card \'' . $card->name . '\' with id ' . $taskId . PHP_EOL;
+
+	addComments($cardActions, $taskId);
 
 	if ($card->badges->checkItems > 0) {
 		$jsonString = 
 			file_get_contents("https://trello.com/1/cards/".
 				$card->shortLink."/checklists?key=".$trellokey."&token=".$trellotoken);
 		$cardDetails = json_decode($jsonString);
+		if (empty($cardDetails)) {
+			printf($jsonString);
+			echo PHP_EOL;
+			printf('Unable to parse JSON response for checklists, is it valid? %s', json_last_error_msg());
+			echo PHP_EOL;
+			die(1);
+		}
 		addCheckItems($cardDetails, $taskId);
 	}
 
@@ -200,6 +343,13 @@ global $userId;
 			file_get_contents("https://trello.com/1/cards/".
 				$card->shortLink."/attachments?key=".$trellokey."&token=".$trellotoken);
 		$cardDetails = json_decode($jsonString);
+		if (empty($cardDetails)) {
+			printf($jsonString);
+			echo PHP_EOL;
+			printf('Unable to parse JSON response for attachments, is it valid? %s', json_last_error_msg());
+			echo PHP_EOL;
+			die(1);
+		}
 		addAttachments($card, $cardDetails, $taskId, $projectId);
 	}
 }
@@ -207,37 +357,69 @@ global $userId;
 //download attachments
 function addAttachments($card, $cardDetails, $taskId, $projectId)
 {
+global $trellokey;
+global $trellotoken;
 global $userId;
 global $client;
+global $addTrelloUserInfo;
+global $dateFormatter;
+global $attachmentCount;
+global $attachmentTotalSizeInBytes;
+
+	echo "      Adding " . count($cardDetails) . " attachments..." . PHP_EOL;
 
 	foreach ($cardDetails as $attachment) {
 		if ($attachment->isUpload) {
 			$filename = $taskId . '_' . $attachment->name;
-			printf('Downloading attachment for task %s to %s.%s', $card->name, $filename, PHP_EOL);
+			printf('        Downloading attachment for task %s to %s.%s', $card->name, $filename, PHP_EOL);
 
 			//Here is the file we are downloading, replace spaces with %20
+			echo "        Downloading from " . $attachment->url . PHP_EOL;
 			$ch = curl_init($attachment->url);
 		 
-			curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 		 
 			//return file in variable
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, ["Authorization: OAuth oauth_consumer_key=\"$trellokey\", oauth_token=\"$trellotoken\""]);
 		 
 			$data = curl_exec($ch);//get curl response
-			if ($data === false) {
+			if ($data === false || curl_error($ch)) {
 				printf('Unable to download attachment: %s%s', curl_error($ch), PHP_EOL);
+				die(1);
 			}
 
-			//done
-			curl_close($ch);
-
 			// upload file as an attachment
-			$client->createTaskFile(array('task_id' => $taskId, 'filename' => $filename, 'project_id' => $projectId, 'blob' => base64_encode($data)));
+			$data_size = strlen($data);
+			$blob = base64_encode($data);
+			$blobSize = strlen($blob);
+			echo "        Uploading $filename for task=$taskId projectId=$projectId data_size=$data_size blob size=$blobSize" . PHP_EOL;
+			$attachmentCount += 1;
+			$attachmentTotalSizeInBytes += $data_size;
+
+			$fileId = $client->createTaskFile(array('task_id' => $taskId, 'filename' => $filename, 'project_id' => $projectId, 'blob' => $blob));
+			if ($fileId === false) {
+				echo "Error uploading file!" . PHP_EOL;
+				die(1);
+			}
+			echo "        Attachment (fileId=$fileId) created" . PHP_EOL;
 		} else {
 			// just an url, add a comment
 			$text = $attachment->url;
-			$client->createComment(array('task_id' => $taskId, 'user_id' => $userId, 'content' => $text));
+			$originalAuthor = "";
+			if ($addTrelloUserInfo === TRUE) {
+				$memberDetails = resolveTrelloUserId($attachment->idMember);
+				$formattedDate = $dateFormatter->format(new DateTimeImmutable($attachment->date));
+				$originalAuthor = "\n\n--\nCreated by {$memberDetails} on {$formattedDate}";
+			}
+			$commentId = $client->createComment(array('task_id' => $taskId, 'user_id' => $userId, 'content' => $text . $originalAuthor));
+			if ($commentId === false) {
+				echo "Error creating comment for attachment (task_id=$taskId, user_id=$userId, content=$text)!" . PHP_EOL;
+				print_r($attachment);
+				die(1);
+			}
+			echo "        Created comment for attachment (id=$commentId)" . PHP_EOL;
 		}
 	}
 }
@@ -251,23 +433,50 @@ global $client;
 $statusTodo = 0;
 $statusDone = 2;
 
+	echo "      Adding " . count($cardDetails) . " check lists" . PHP_EOL;
 	foreach ($cardDetails as $checkList) {
 		foreach ($checkList->checkItems as $checkItem) {
 			$title = $checkList->name . ' - ' . $checkItem->name;
 			$status = $checkItem->state === 'incomplete' ? $statusTodo : $statusDone;
-			$client->createSubtask(array('task_id' => $taskId, 'title' => $title, 'status' => $status));
+			$subtaskId = $client->createSubtask(array('task_id' => $taskId, 'title' => $title, 'status' => $status));
+			if ($subtaskId === false) {
+				echo "Error creating subtask! (task_id=$taskId, title=$title, status=$status)" . PHP_EOL;
+				die(1);
+			}
 		}
 	}
 }
 
-function addComments($cardDetails, $taskId)
+function addComments($cardActions, $taskId)
 {
 global $userId;
 global $client;
-	foreach ($cardDetails as $comment) {
-		if ($comment->type === 'commentCard') {
+global $addTrelloUserInfo;
+global $dateFormatter;
+	$commentCount = 0;
+	foreach ($cardActions as $action) {
+		if ($action->type === 'commentCard' || $action->type === 'copyCommentCard') {
+			$commentCount += 1;
+		}
+	}
+	if ($commentCount === 0) {
+		return;
+	}
+
+	echo "      Adding {$commentCount} comments..." . PHP_EOL;
+	foreach ($cardActions as $comment) {
+		if ($comment->type === 'commentCard' || $comment->type === 'copyCommentCard') {
 			$text = $comment->data->text;
-			$client->createComment(array('task_id' => $taskId, 'user_id' => $userId, 'content' => $text));
+			$originalAuthor = "";
+			if ($addTrelloUserInfo === TRUE) {
+				$originalAuthor = "\n\n--\nComment created " . formatActionMemberCreator($comment);
+			}
+			$commentId = $client->createComment(array('task_id' => $taskId, 'user_id' => $userId, 'content' => $text . $originalAuthor));
+			if ($commentId === false) {
+				echo "Error creating comment! (task_id=$taskId, user_id=$userId, content length=" . strlen($text) . ")" . PHP_EOL;
+				die(1);
+			}
+			echo "        Created comment (id=$commentId)" . PHP_EOL;
 		}
 	}
 }
